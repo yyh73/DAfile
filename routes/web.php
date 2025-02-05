9704e0430ffc276aa0634013eb32ff896727c6e1
yyh73 <9874yiru@gmail.com>
2025-02-05 23:16:57 +0000

    Calendar View

--- routes/web.php
+++ routes/web.php
@@ -25,9 +25,19 @@ Route::get('/dashboard', function () {
 
 Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-    Route::get('/calendar', [EventController::class, 'show'])->name("show");
+    Route::get('/calendar', [EventController::class, 'show'])->name("show"); // カレンダー表示
+    
+    Route::post('/calendar/create', [EventController::class, 'create'])->name("create"); // 予定の新規追加
+
+    Route::put('/calendar/update', [EventController::class, 'update'])->name("update"); // 予定の更新
+
+    Route::post('/calendar/get',  [EventController::class, 'get'])->name("get"); // DBに登録した予定を取得
+
+    Route::delete('/calendar/delete', [EventController::class, 'delete'])->name("delete"); // 予定の削除
 });
 
 require __DIR__.'/auth.php';
